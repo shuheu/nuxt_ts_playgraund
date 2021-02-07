26d3803631c9fa9aa2fb4c74e83884f2b0a3846d
shuhei <shhxshh.1010@gmail.com>
2021-02-07 23:38:50 +0900

    plugin で errorメソッドを使いたい

--- app/plugins/http_client.js
+++ app/plugins/http_client.js
@@ -1,36 +1,43 @@
 
-export default function ({ $axios }, inject) {
+export default function ({ $axios, redirect, error: nuxtError }, inject) {
   // Create a custom axios instance
   const htttp = $axios.create({
     headers: {
       common: {
         Accept: 'text/plain, */*',
         'X-Requested-With': 'XMLHttpRequest',
-      }
+      },
+
+    },
+    onRequest: {
+
     }
   })
-
   // Set baseURL to something different
   htttp.setBaseURL('https://jsonplaceholder.typicode.com')
-
-  htttp.onError(error => {
+  htttp.onResponseError(error => {
     const code = parseInt(error.response && error.response.status)
     if (code === 404) {
       console.log("リダイレクトします。")
-      redirect('/')
+      nuxtError({
+        statusCode: 404,
+        message: 'エラー'
+      })
+
+    } else if (code == 503) {
+      console.log("メンテナスです")
+      nuxtError({
+        statusCode: 503,
+        message: 'メンテナンスや'
+      })
     }
   })
-
   htttp.onRequest(config => {
+    // console.log(context)
     console.log('Making request to ' + config.url)
     // console.log('request: ' + request)
-    htttp.setToken("aaaaaaaaaaa")
+    htttp.setToken("aaaaaaaaaaa") //cookiesから取得する。
   })
-
-
-
-
-
   // onRequestの処理追加
   // authorization にcookieから取得処理走らせる。
 
